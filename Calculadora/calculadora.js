c14418d201d4dfa71181a331093dbcd576c1d804
alvaro05p <alvaro05pomg@gmail.com>
2024-10-08 16:54:03 +0200

    evitar simbolor consecutivos

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,19 +1,61 @@
-
-
 function apretar(elemento){
     elemento.classList.toggle("sombra");
 }
 
 let pantalla = document.getElementById("pantallaInput")
 
-let mostrar = ""
+let teclasCalculadora = [
+    // Números
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+
+    // Operadores
+    '+', '-', '*', '/', '%', '.',
+
+    // Teclas especiales
+    'Enter',    // Equivalente a igual (=)
+    'Backspace' // Para borrar el último dígito
+]
+
+document.addEventListener('keydown', function(event) {
+    console.log(event.key);
+    if(teclasCalculadora.includes(event.key)){
+        introducir(event.key);
+    }
+
+    
+})
+
+let mostrar = "";
+
+let operandos = [];
 
 function introducir(elemento){
 
-    console.log(elemento.innerHTML)
     valorElemento = elemento.innerHTML.trim();
+
+    let caracteres = ["+","-","x","/",".","%"];
+
+    if(caracteres.includes(mostrar[mostrar.length-1]) && caracteres.includes(valorElemento)){
+        return;
+    }
+
     mostrar += valorElemento;
-    console.log(mostrar);
     pantalla.value=mostrar;
+}
+
+function cero(){
+    pantalla.value = 0;
+    mostrar="";
+}
+
+function borrar(){
     
+    mostrar = mostrar.substring(0, mostrar.length - 1);
+    pantalla.value=mostrar;
+}
+
+function igual(){
+    mostrar = mostrar.replace("x", "*");
+    mostrar = eval(mostrar);
+    pantalla.value = mostrar;
 }
