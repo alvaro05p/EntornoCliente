5258bdba2c28f71af6a26f07f80925278fc5b339
alvaro05p <alvaro05pomg@gmail.com>
2024-10-14 19:56:43 +0200

    wordle

---
+++ Wordle/scripts.js
@@ -0,0 +1,9 @@
+
+
+document.querySelectorAll(".key").forEach(function(button){
+    button.addEventListener("click",function(){
+        let valor = button.textContent.trim();
+
+        if(valor != "")
+    });
+})
