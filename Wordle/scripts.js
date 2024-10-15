2472dd0f2957d5dc2eefe7db762bc1f84a6c88d8
alvaro05p <alvaro05pomg@gmail.com>
2024-10-15 16:06:10 +0200

    del y enter

--- Wordle/scripts.js
+++ Wordle/scripts.js
@@ -1,6 +1,7 @@
 let filaActual = 0;
 let colActual = 0;
 
+seleccionada();
 
 document.querySelectorAll(".key").forEach(function(button){
     button.addEventListener("click",function(){
@@ -22,6 +23,13 @@ function recopilar(valor){
     let filas = document.getElementsByClassName("row");
 
     let filaSel=filas[filaActual];
+
+    for (let i = 0; i < filaSel.length; i++) {
+        filaSel[i].classList.toggle("selec");
+        if(i!=0){
+            filaSel[i-1].classList.toggle("selec");
+        }
+    }
 
     //Todas las columnas de fila
     let columnas = filaSel.getElementsByClassName("cell");
@@ -58,7 +66,32 @@ function del(){
 function enter(){
     filaActual++;
     colActual=0;
+    seleccionada();
+
 }
+
+function seleccionada(){
+
+    //Filas
+    let filas = document.getElementsByClassName("row");
+
+    let filaSel=filas[filaActual];
+
+    //Todas las columnas de fila
+    let columnas = filaSel.getElementsByClassName("cell");
+
+    for (let i = 0; i < columnas.length; i++) {
+        columnas[i].classList.toggle("selec");  
+    }
+
+    if (colActual > 0) {
+        let prevCol = filaSel.getElementsByClassName("cell")[colActual - 1];
+        prevCol.classList.toggle("selec");
+    }
+
+}
+
+
 
 
 
