8eb47c1cc05de8337d050a96a6be7cabed712c88
alvaro05p <alvaro05pomg@gmail.com>
2024-10-24 08:46:09 +0200

    corretas e incorrectas

--- pelotasElJuego/scripts.js
+++ pelotasElJuego/scripts.js
@@ -2,7 +2,9 @@ let jugar = document.getElementById("jugar");
 let selectPelotas = document.getElementById("numPelotas");
 let valorSelect;
 let correctas = document.getElementById("correctas");
-contador = 0;
+let incorrectas = document.getElementById("incorrectas");
+let contador = 0;
+let contador2 = 0;
 
 selectPelotas.addEventListener("change", function(){
 
@@ -22,17 +24,23 @@ jugar.addEventListener("click", function(){
         let altura = Math.random() * 435;
         let horizontal = Math.random() * 1100;
         let colorNum = Math.floor((Math.random() * 2) + 1);
-        let color;
+        let color="";
+        let index="";
         console.log(colorNum);
 
         switch(colorNum){
             case 1:
                 color = "blue";
+                index = "1";
+                break;
             case 2:
                 color = "red";
+                break;
         }
 
         let circulo = document.createElement("div");
+
+        console.log(color);
 
         circulo.style.backgroundColor = color;
         circulo.style.padding = size + "px";
@@ -42,12 +50,21 @@ jugar.addEventListener("click", function(){
         circulo.style.position = "absolute";
         circulo.style.left = horizontal + "px";
         circulo.style.top = altura + "px";
+        circulo.style.zIndex = index;
         areaJuego.appendChild(circulo);
 
         circulo.addEventListener("click", function(){
+            
+
+            if(circulo.style.backgroundColor == "blue"){
+                contador++;
+                correctas.textContent = contador;
+            }else{
+                contador2++;
+                incorrectas.textContent = contador2;
+            }
+
             circulo.remove();
-            contador++;
-            correctas.textContent = contador;
             
         })
     }
