6d1c8178bef657628469ff11b7c4a602d1e20f99
alvaro05p <alvaro05pomg@gmail.com>
2025-01-15 20:06:33 +0100

    forecast eeuu

--- COVID/js/scripts.js
+++ COVID/js/scripts.js
@@ -1,21 +1,35 @@
 areas = document.querySelectorAll("area");
+modal = document.getElementById("modal");
+
+function cerrar(){
+    modal.classList.add("oculto")
+}
 
 areas.forEach(area => {
     area.addEventListener("click", function(){
-        let titulo = this.title;
-        let modal = document.getElementById("modal");
-        let tituloModal = document.getElementById("titulo");
-        tituloModal.textContent = titulo;
-        let datacod = area.getAttribute("data-cod");
+        
         modal.classList.remove("oculto");
-        fetch('https://api.covidtracking.com/v1/states/' + datacod + '/info.json')
-        .then(response => response.json())
-        .then(json => {
-            
+        console.log("oculto");
+        let titulo = document.getElementById("titulo");
+        titulo.textContent=this.title;
+        let coords = this.getAttribute("data-coordinates");
+        
+        fetch("https://api.weatherusa.net/v1/forecast?q=" + coords + "&daily=1&units=e")
+        .then(response => response.json()) 
+        .then(data => {
+            const miercoles = data[0];
+            const descripcion = miercoles.wx_str;
+            let descMiercoles = document.getElementById("miercoles");
+            descMiercoles.textContent = descripcion;
         })
 
-    });
+    })
+
+    
+
+    
+
+
+
 });
-
-
 
