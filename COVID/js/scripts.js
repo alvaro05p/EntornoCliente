b8456aa8568611389af73208ec3c7f047527ec3e
alvaro05p <alvaro05pomg@gmail.com>
2025-01-16 15:51:47 +0100

    forecast funcionando

--- COVID/js/scripts.js
+++ COVID/js/scripts.js
@@ -17,10 +17,38 @@ areas.forEach(area => {
         fetch("https://api.weatherusa.net/v1/forecast?q=" + coords + "&daily=1&units=e")
         .then(response => response.json()) 
         .then(data => {
-            const miercoles = data[0];
-            const descripcion = miercoles.wx_str;
-            let descMiercoles = document.getElementById("miercoles");
-            descMiercoles.textContent = descripcion;
+            let dia = data[0];
+            let descripcion = dia.day_name;
+            let weather = dia.wx_str;
+            let iconDesc = dia.wx_icon;
+            let descDia = document.getElementById("dia1");
+            let descWeather = document.getElementById("weather1");
+            descDia.textContent = descripcion;
+            descWeather.textContent = weather;
+            let icon = document.getElementById("icon1");
+            icon.src = "./icons/"+ iconDesc +".png";
+
+            dia = data[1];
+            descripcion = dia.day_name;
+            weather = dia.wx_str;
+            iconDesc = dia.wx_icon;
+            descDia = document.getElementById("dia2");
+            descWeather = document.getElementById("weather2");
+            descDia.textContent = descripcion;
+            descWeather.textContent = weather;
+            icon = document.getElementById("icon2");
+            icon.src = "./icons/"+ iconDesc +".png";
+
+            dia = data[2];
+            descripcion = dia.day_name;
+            weather = dia.wx_str;
+            iconDesc = dia.wx_icon;
+            descDia = document.getElementById("dia3");
+            descWeather = document.getElementById("weather3");
+            descDia.textContent = descripcion;
+            descWeather.textContent = weather;
+            icon = document.getElementById("icon3");
+            icon.src = "./icons/"+ iconDesc +".png";
         })
 
     })
