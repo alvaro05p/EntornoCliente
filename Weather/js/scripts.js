7bd3a0f7e649709a27e95a85f16891ea87a654e3
alvaro05p <alvaro05pomg@gmail.com>
2025-01-24 17:58:28 +0100

    cambio de url

--- Weather/js/scripts.js
+++ Weather/js/scripts.js
@@ -12,48 +12,40 @@ areas.forEach(area => {
         
         modal.classList.remove("oculto");
         fondo.classList.remove("oculto");
-        console.log("oculto");
         let titulo = document.getElementById("titulo");
         titulo.textContent=this.title;
         let id = this.getAttribute("data-cod");
         let valoresArray = coordenadas.find(item => item.code === id);
         let coords = valoresArray.coordinates;
-        console.log(coords);
-        fetch("https://api.weatherusa.net/v1/forecast?q=" + coords + "&daily=1&units=e")
+        //console.log(coords);
+        fetch("https://api.weatherusa.net/v1/forecast?q=" + coords + "&hourly=2&units=e")
         .then(response => response.json()) 
         .then(data => {
-            let dia = data[0];
-            let descripcion = dia.day_name;
-            let weather = dia.wx_str;
-            let iconDesc = dia.wx_icon;
-            let descDia = document.getElementById("dia1");
-            let descWeather = document.getElementById("weather1");
-            descDia.textContent = descripcion;
-            descWeather.textContent = weather;
-            let icon = document.getElementById("icon1");
-            icon.src = "./icons/"+ iconDesc +".png";
 
-            dia = data[1];
-            descripcion = dia.day_name;
-            weather = dia.wx_str;
-            iconDesc = dia.wx_icon;
-            descDia = document.getElementById("dia2");
-            descWeather = document.getElementById("weather2");
-            descDia.textContent = descripcion;
-            descWeather.textContent = weather;
-            icon = document.getElementById("icon2");
-            icon.src = "./icons/"+ iconDesc +".png";
+            
 
-            dia = data[2];
-            descripcion = dia.day_name;
-            weather = dia.wx_str;
-            iconDesc = dia.wx_icon;
-            descDia = document.getElementById("dia3");
-            descWeather = document.getElementById("weather3");
-            descDia.textContent = descripcion;
-            descWeather.textContent = weather;
-            icon = document.getElementById("icon3");
-            icon.src = "./icons/"+ iconDesc +".png";
+            data.forEach(element => {
+                console.log(element.localtime);
+            });
+                let hora = data[1];
+                
+                let descripcion = hora.localtime;
+                console.log(descripcion);
+                /*
+                let weather = dia.wx_str;
+                let iconDesc = dia.wx_icon;
+                let descDia = document.getElementById("dia1");
+                let descWeather = document.getElementById("weather1");
+                descDia.textContent = descripcion;
+                descWeather.textContent = weather;
+                let icon = document.getElementById("icon1");
+                icon.src = "./icons/"+ iconDesc +".png";
+                */
+            
+
+            
+
+            
         })
 
     })
@@ -116,4 +108,7 @@ areas.forEach(area => {
 
 
 });
+
+
+
 
